f25b2956e7e00d13392a16c9b5972581abbabe97
excelitai-mostofa <mostofa@excelitai.com>
2022-04-05 15:18:46 +0600

    Page Controller Added

--- lib/on_boarding_page.dart
+++ lib/on_boarding_page.dart
@@ -11,43 +11,79 @@ class OnBoardingPage extends StatefulWidget {
 }
 
 class _OnBoardingPageState extends State<OnBoardingPage> {
+  Container dotIndicator(index) {
+    return Container(
+      margin: EdgeInsets.only(left: 5),
+      height: 10,
+      width: 10,
+      decoration: BoxDecoration(color: Colors.amber, shape: BoxShape.circle),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     SizeConfigure().init(context);
     double sizeV = SizeConfigure.blockSizeV!;
     double sizeH = SizeConfigure.blockSizeH!;
+    int currentPageIndex = 0;
 
+    PageController _pageController = PageController(initialPage: 0);
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
           child: Column(
         children: [
           Expanded(
+            flex: 20,
             child: PageView.builder(
+              controller: _pageController,
               itemCount: splashScreenContent.length,
-              itemBuilder: (context, index) =>
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: sizeV*50,
-                        child: Image.asset(splashScreenContent[index].image,fit: BoxFit.cover,),
-                      ),
-                      SizedBox(
-                        height: sizeV*2,
-                      ),
-                      Text(splashScreenContent[index].title,style: kTitle,textAlign:TextAlign.center,),
-
-                      SizedBox(
-                        height: sizeV*2.7,
-                      ),
-
-                      Text(splashScreenContent[index].subTitle,style: kBodyText1,textAlign:TextAlign.center),
-                    ],
+              itemBuilder: (context, index) => Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: sizeV * 40,
+                    child: Image.asset(
+                      splashScreenContent[index].image,
+                      fit: BoxFit.cover,
+                    ),
                   ),
+                  SizedBox(
+                    height: sizeV * 4,
+                  ),
+                  Text(
+                    splashScreenContent[index].title,
+                    style: kTitle,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: sizeV * 4,
+                  ),
+                  Text(splashScreenContent[index].subTitle,
+                      style: kBodyText1, textAlign: TextAlign.center),
+                ],
+              ),
             ),
-          )
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(onPressed: () {}, child: Text('Skip')),
+                Row(
+                    children: List.generate(
+                  splashScreenContent.length,
+                  (index) => dotIndicator(index),
+                )),
+                TextButton(onPressed: () {}, child: Text('Next')),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: sizeV * 5,
+          ),
         ],
       )),
     );
